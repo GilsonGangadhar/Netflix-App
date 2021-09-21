bc11d6410018141bbded54929249541f26d5b6f3
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-09-22 01:43:28 +0530

    initial homepage & navbar setting(avatar, logo)

---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+    return (
+        <div className="nav" >
+            <div className="nav_contents">
+             <img 
+                className="nav_logo"
+                src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024" alt="" />
+
+
+                <img
+                className="nav_avatar"
+                src="https://p.kindpng.com/picc/s/116-1169050_avatar-michael-jordan-jersey-clip-art-michael-jordan.png" alt="" />
+            </div>       
+        </div>
+    )
+}
+
+export default Nav
