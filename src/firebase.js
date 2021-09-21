7048ff12a322748a5c8fb9d5c8d3518fe81882ac
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-09-22 00:46:44 +0530

    initial commit & firebase-setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEdWRnB-Vf4RJRCTwcWCcQ18pfkWyHogE",
+    authDomain: "netflix-app-9bdb4.firebaseapp.com",
+    projectId: "netflix-app-9bdb4",
+    storageBucket: "netflix-app-9bdb4.appspot.com",
+    messagingSenderId: "885981580326",
+    appId: "1:885981580326:web:0fa7649a021af6c1636ae7"
+  };
