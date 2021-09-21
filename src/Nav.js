a0e13fce0f2345766f4fc830e7bc1fb3c3a4b350
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-09-22 02:25:55 +0530

    initial navbar, avatar & banner setting

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Nav.css'
 
 function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if(window.scrollY > 100) {
+            handleShow(true)
+        } else {
+            handleShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return() => window.removeEventListener("scroll", transitionNavBar)
+    }, [])
+
     return (
-        <div className="nav" >
+        <div className={`nav ${show & "nav_black"}` }>
             <div className="nav_contents">
              <img 
                 className="nav_logo"
-                src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024" alt="" />
+                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
 
 
                 <img
