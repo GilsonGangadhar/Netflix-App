84afd8ee4d88fe6fb7eabeaf2147d205071f78a7
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-09-22 17:53:27 +0530

    added login & signUp screens

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './LoginScreen.css'
+import SignupScreen from './SignupScreen'
 
 /*https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png​. below link for netflix logo */
 
 function LoginScreen() {
+
+    const [signIn, setSignIn] = useState(false);
+
+
     return (
         <div className="loginScreen" >
             <div className="loginScreen_background" >
 
                 <img className="loginScreen_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png" alt="" />
 
-                <button className="loginScreen_button" >Sign In</button>
+                <button onClick={() => setSignIn(true)} className="loginScreen_button" >Sign In</button>
 
                 <div className="loginScreen_gradient" > </div>
 
                 <div className="loginScreen_body" >
-                    <>
+                    {signIn ? (
+                        <SignupScreen />
+                    ) : (
+                        <>
                         <h1>Unlimited films, TV programs and more.</h1>
                         <h2>Watch anywhere. cancel anytime</h2>
                         <h3>Ready to watch? Enter yout email to create or restart your membership</h3>
@@ -23,10 +31,13 @@ function LoginScreen() {
                         <div className="loginScreen_input" >
                             <form>
                                 <input type="email" placeholder="Enter your e-mail address"  />
-                                <button className="loginScreen_getStarted" >GetStarted</button>
+                                <button onClick={() => setSignIn(true)} className="loginScreen_getStarted" >GetStarted</button>
                             </form>
                         </div>
                     </>
+                    )}
+                    
+ 
                 </div>
             </div>
         </div>
