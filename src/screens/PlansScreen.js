64e866d4759948302d4be9d71c72d9a480586405
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-09-24 01:41:52 +0530

    connecting subscription page to billing page and back

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -9,6 +9,22 @@ function PlansScreen() {
 
     const [products, setProducts] = useState([])
     const user = useSelector(selectUser)
+    const [subscription, setSubscription] = useState(null)
+
+    useEffect(() => {
+        db.collection('customers').doc(user.uid).collection('subscription').get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(async subscription => {
+                setSubscription({
+                    role : subscription.data().role,
+                    current_period_end : subscription.data().current_period_end.seconds,
+                    current_period_start : subscription.data().current_period_start
+                })
+            })
+        })
+    }, [user.uid])
+
+    
 
     useEffect(() => {
         db.collection('products').where('active', '==', true).get()
@@ -16,10 +32,11 @@ function PlansScreen() {
             const products = {}
             querySnapshot.forEach(async (productDoc) => {
                 products[productDoc.id] = productDoc.data();
-                const priceSnap = await productDoc.ref.collection("prices").get();
+                const priceSnap = await productDoc.ref.collection('prices').get();
                 priceSnap.docs.forEach(price =>{
                     products[productDoc.id].prices = {
-                        priceId : price.data()
+                        priceId : price.id,
+                        priceData : price.data()
                     }
                 })
             })
@@ -27,10 +44,11 @@ function PlansScreen() {
         })
     }, [])
 
+    console.log(products, "useEffect") 
+
 
     const loadCheckout = async (priceId) => {
-
-        const docRef =  await db.collection("customers").doc(user.uid).collection("checkout_sessions").add({
+        const docRef =  await db.collection('customers').doc(user.uid).collection("checkout_sessions").add({
             price : priceId,
             success_url : window.location.origin, 
             cancel_url : window.location.origin
@@ -45,30 +63,30 @@ function PlansScreen() {
         }
 
         if(sessionId){
-            //it redirects to checkout
+            //it redirects to checkout 
 
-            const stripe = await loadStripe('pk_test_51JcprgSJkRbIPmLKePzsPnCLTt9scF3EfGrDtLSD0JSju3w8UhmWr7bWcs7jmfDpyxPd249d5dhGx6cNm5Xk3rBi001b2vm1Lh')
+            const stripe = await loadStripe("pk_test_51JcprgSJkRbIPmLKePzsPnCLTt9scF3EfGrDtLSD0JSju3w8UhmWr7bWcs7jmfDpyxPd249d5dhGx6cNm5Xk3rBi001b2vm1Lh")
 
             stripe.redirectToCheckout({sessionId})
         }
     })
     };
 
-    // console.log(Object.entries(products), "products")
+    
     // console.log(productData.prices.priceId)
 
     return (
         <div className="plansScreen" >
             {Object.entries(products).map(([productId, productData]) => {
                 //add some logic to check if the user subscribtion is active...
-                console.log(productData.prices, "priceId")
                 return (
                     <div className="plansScreen_plan" >
                         <div className="plansScreen_Info">
                             <h5>{productData.name}</h5>
                             <h6>{productData.description}</h6>
                         </div>
-                        <button onClick={() => loadCheckout(productData.prices.priceId)} >Subscribe</button>
+
+                        <button onClick={() => loadCheckout(productData.prices.priceId)}>Subscribe</button>
                     </div>
                 )
             })}
